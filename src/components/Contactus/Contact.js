06efc864f0d0804fa0bc3636d739a09e9aac3191
samranaaz <samranaaz81@gmail.com>
2023-05-22 02:52:50 -0700

    contact us page has been complete

--- src/components/Contactus/Contact.js
+++ src/components/Contactus/Contact.js
@@ -3,13 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div class="container-lg  h-auto w-auto bg-light rounded p-3 ">
-      <div className='row'>
-        <div className=''>
-           {/* <img src="./contact.jpeg" alt="image" className=' img-fluid h-100% w-100%'/> */}
-
-
-        </div>
-      </div>
+      
     <div class="row">
       <div className='col-md-6 bg-success'>
       <img src="./contact.jpeg" alt="image" className=' img-fluid h-100% w-100%'/>
@@ -24,13 +18,30 @@ const Contact = () => {
           <p class="text-md-center  mx-auto mt-4 mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
            a matter of hours to help you.</p>
           </div>
+          <div className='col'>
+          <ul class="list-unstyled mb-0 mt-4 ms-4 me-4 text-center">
+                <li><i class="bi bi-geo-alt bi-2x"></i>
+                    <p>Askari Tower, Lahore, Pakistan</p>
+                </li>
+
+                <li>
+                <i class="bi bi-telephone-inbound-fill mt-4 bi-2x"></i>
+                    <p>+ 01 234 567 89</p>
+                </li>
+
+                <li>
+                <i class="bi bi-envelope-at-fill mt-4 ba-2x"></i>
+                    <p>SLEWBITES1234@GMAIL.COM</p>
+                </li>
+            </ul>
+          </div>
          
       </div>
       <div class="col-md-3  align-items-center">
         <div class="">
           <div class="form-group">
             <h1>contact us</h1>
-            <label class="control-label " for="fname">First Name:</label>
+            <label class="control-label mt-4 " for="fname">First Name:</label>
             <div class="col-sm-10">          
             <input type="text" class="form-control" id="fname" placeholder="Enter First Name" name="fname"/>
             </div>
@@ -54,7 +65,7 @@ const Contact = () => {
             </div>
           </div>
           <div class="form-group">        
-            <div class="col-sm-offset-2 mt-2  ms-4">
+            <div class="col-sm-offset-2 mt-3  ms-4">
             <button type="submit" class="btn btn-success">Submit</button>
             </div>
           </div>
