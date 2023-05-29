47687beb399a9e13574140a4230224d611a1ac3a
samranaaz <samranaaz81@gmail.com>
2023-05-29 12:09:24 -0700

    update contact.js

--- src/components/Contactus/Contact.js
+++ src/components/Contactus/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
@@ -19,40 +19,50 @@ const ContactUs = () => {
         message: Yup.string().required('Message is required'),
     });
 
+    const [successMessage, setSuccessMessage] = useState('');
+
     const handleSubmit = async (values) => {
         try {
-            // Send the form data to the backend server
-            const response = await fetch('http://localhost:3000/api/contactus', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(values),
+            // Handle form submission here (e.g., send the form data to your server)
+            console.log(values); // You can access the form data here
+
+            // Simulating an asynchronous request
+            const response = await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    // Assuming the request is successful
+                    const successResponse = {
+                        status: 'success',
+                        message: 'Query submitted successfully!',
+                    };
+                    resolve(successResponse);
+
+                    // Rejecting the request
+                    reject('Error: Failed to submit the query');
+                }, 2000); // Simulating a delay of 2 seconds
             });
 
-            if (response.ok) {
-                console.log('Form data sent successfully');
-                // Do something after successful form submission
-            } else {
-                console.log('Failed to send form data');
-                // Handle the error case
-            }
+            console.log(response); // Handle the response as per your requirement
+            setSuccessMessage(response.message); // Set the success message
+            // alert("form data submitted to backend successfully")
+            // Clear the form values after successful submission
+            // You can remove this if you want to keep the values in the form
+            values.name = '';
+            values.email = '';
+            values.message = '';
         } catch (error) {
-            console.log('Error occurred while sending form data', error);
-            // Handle the error case
+            console.error(error); // Handle the error case
+            alert('Error: Failed to submit the query'); // Display error message (you can modify this as needed)
         }
     };
 
     return (
         <div className="contact-us-container">
             <Container>
-                <Row className="justify-content-center  mt-5">
-                    <Col md={8} className='shadow'>
-                        <Row className="align-items-center bg-light p-3">
+                <Row className="justify-content-center mt-5">
+                    <Col md={8}>
+                        <Row className="align-items-center bg-light">
                             <Col md={2}>
-                                <img src='./contact.jpeg' alt="SLEWBITES Logo" className="img-fluid"
-
-                                    style={{ maxWidth: '100%' }} />
+                                <img src='./contact.jpeg' alt="SLEWBITES Logo" className="img-fluid" style={{ maxWidth: '100%' }} />
                             </Col>
                             <Col md={10}>
                                 <h3>SlewBites</h3>
@@ -101,18 +111,24 @@ const ContactUs = () => {
                                             as="textarea"
                                             name="message"
                                             rows={4}
-                                            className="form-control "
+                                            className="form-control"
                                         />
                                         <ErrorMessage
                                             name="message"
                                             component="div"
-                                            className="text-danger "
+                                            className="text-danger"
                                         />
                                     </Form.Group>
 
                                     <Button variant="success" type="submit" className='mt-3 rounded p-2'>
                                         Please Submit your Query
                                     </Button>
+
+                                    {successMessage && (
+                                        <div className="alert alert-success mt-3" role="alert">
+                                            {successMessage}
+                                        </div>
+                                    )}
 
                                     <div className="mt-3 d-flex justify-content-center bg-light p-4">
                                         <a href="https://www.facebook.com">
